7c6b34472dd690262057ed180e5bc82faaa45458
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-05-25 22:07:12 -0600

    Update Serie.h
    Implementacion de algunos metodos de la clase, asimismo implementacion de herencia entre Serie y Video.

--- Serie.h
+++ Serie.h
@@ -1,16 +1,24 @@
 #ifndef Serie_
 #define Serie_
 #include "Episodio.h"
+#include "Video.h"
 #include <iostream>
 
 using namespace std;
 
-class Serie:public Video{
+class Serie: public Video{
     private:
     Episodio capitulo;
 
     public:
     string series[100];
+    Serie(string Tit, string gnro, string identi, float estrellas):Video(Tit, gnro,identi){
+
+    }
+    Serie(string Tit,string gnro,string identi,float estrellas,float drcnmin): Video(Tit, gnro,identi,estrellas,drcnmin){
+        
+    }
 };
+
 
 #endif
