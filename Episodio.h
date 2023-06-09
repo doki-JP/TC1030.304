ed2e32fdd57f5a63f488289b747d6d3b85e4b422
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-08 21:50:08 -0600

    Update Episodio.h
    Se modificó el planteamiento del código, haciendo que el UML esté mucho más alejado a lo diseñado y requiere una actualización inmediata

--- Episodio.h
+++ Episodio.h
@@ -5,16 +5,24 @@ using namespace std;
 
 class Episodio{
     private:
-    string NumCapitulo;
-    string NumTemporada;
+    string NumCapitulos;
+    string NumTemporadas;
     string Titulo;
 
     public:
-    void AñadirCapTemp(string cap, string temp){}
-    void AñadirTitulo(string titu){}
-    string ObtenerCapTemp(){return NumCapitulo,NumTemporada;}
+    void AñadirCap(int cap);
+    void AñadirTemp(int temp);
+    void AñadirTitulo(string titu);
+    string ObtenerTempCap(){return NumTemporadas,NumCapitulos;}
     string ObtenerTitulo(){return Titulo;}
-
 };
-
+void Episodio::AñadirCap(int cap){
+    NumCapitulos=cap;
+}
+void Episodio::AñadirTemp(int temp){
+    NumTemporadas=temp;
+}
+void Episodio::AñadirTitulo(string titu){
+    Titulo=titu;
+}
 #endif
