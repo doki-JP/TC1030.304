70135ef042c4e51fd7c180c47d99b9250c4954b4
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-08 21:46:02 -0600

    Update Video.h
    Se convirtió en una clase abstracta de la que heredan dos clases extra e implementan ese método.

--- Video.h
+++ Video.h
@@ -1,46 +1,42 @@
 #ifndef Videos
 #define videos
 #include <iostream>
+#include "Pelicula.h"
+#include "Serie.h"
 using namespace std;
 
 class Video{
-    private:
+    protected:
         string genero;
         string titulo;
         string ID;
         float duracion_minutos;
         float rating;
     public:
-        Video();
-        Video(string Tit, string gnro, string identi);
-        Video(string Tit, string gnro, string identi, float estrellas,float drcnmin);
-        virtual void AgregaContenido(string){};
-        string BuscarContenido(string ID){};
+        Video(string,string,float,float,string,int,int);
+        Video(string, string,float, float, string);
+        string BuscarContenido(string ID);
         float ObtenerRating(){return rating;};
-        string MostrarVideos(){};
+        virtual void MostrarVideos()=0;
         void AñadirRating(float estrellas){};
         string getGenero(){return genero;};
         string getTitulo(){return titulo;};
         void setTitulo(string tit){titulo=tit;};
         void setGenero(string gnro){genero=gnro;};
 };
-Video::Video(string Tit, string gnro, string identi){
-    titulo =Tit;
-    genero=gnro;
-    ID=identi;
-}
-Video::Video(string Tit, string gnro, string identi, float estrellas, float duracnmin){
+Video::Video(string Tit, string gnro,float drcnmin, float estrellas, string identi,int temp,int capi){
     titulo =Tit;
     genero=gnro;
     ID=identi;
     rating=estrellas;
-    duracion_minutos=duracnmin;
+    duracion_minutos=drcnmin;
 }
-Video::Video(){
-    titulo= "";
-    genero="";
-    ID="";
+Video::Video(string Tit, string gnro,float drcnmin, float estrellas, string identi){
+    titulo =Tit;
+    genero=gnro;
+    ID=identi;
+    rating=estrellas;
+    duracion_minutos=drcnmin;
 }
-
 
 #endif
