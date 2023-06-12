d826c6a3e5cd29fba74180b72ffea23e9a77a418
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-12 09:18:37 -0600

    Update Video.h
    Se añadió el contenido de la clase "Serie" y "Pelicula" a esta clase, además de arreglar algunos problemas con tipos de datos.

--- Video.h
+++ Video.h
@@ -1,8 +1,7 @@
-#ifndef Vide
-#define vide
+#ifndef VIDEO_H
+#define VIDEO_H
+#include "Episodio.h"
 #include <iostream>
-#include "Pelicula.h"
-#include "Serie.h"
 
 class Video {
 protected:
@@ -31,4 +30,47 @@ Video::Video(string Tit, string gnro, float drcnmin, float estrellas, string ide
     duracion_minutos = drcnmin;
 }
 
+class Pelicula : public Video {
+public:
+    Pelicula(string, string, float, float, string);
+    void MostrarVideos() override;
+};
+Pelicula::Pelicula(string tit, string gen, float drcn, float estrellas, string id)
+    : Video(tit, gen, drcn, estrellas, id) {}
+
+void Pelicula::MostrarVideos() {
+    cout << "Titulo: " << titulo << "\n";
+    cout << "Genero: " << genero << "\n";
+    cout << "Rating: " << rating << "\n";
+    cout << "Duracion en minutos: " << duracion_minutos << "\n";
+}
+class Serie : public Video {
+protected:
+    Episodio cap[100];
+    int numC;
+
+public:
+    Serie(string, string, float, float, string, int, int);
+    void MostrarVideos() override;
+    void MostrarCapSerie(string);
+};
+Serie::Serie(string Tit, string gnro, float drcnmin, float estrellas, string identi, int temp, int capi)
+    : Video(Tit, gnro, drcnmin, estrellas, identi) {
+    numC = capi;
+}
+
+void Serie::MostrarVideos() {
+    cout << "Titulo: " << titulo << "\n";
+    cout << "Genero: " << genero << "\n";
+    cout << "Rating: " << rating << "\n";
+    cout << "Duracion en minutos: " << duracion_minutos << "\n";
+}
+
+void Serie::MostrarCapSerie(string nom) {
+    cout << "Los capitulos de la serie son:\n";
+    for (int i = 0; i < numC; i++) {
+        cap[i].MostrarDatosCap();
+        cout << "\n";
+    }
+}
 #endif
