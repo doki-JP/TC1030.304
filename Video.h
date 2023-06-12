c2f4282bb9276dd587ceea6570c25c963c67ddd7
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-12 08:53:45 -0600

    Update Video.h
    eliminación de algunos métodos innecesarios, abstracción completa de la clase Video y ligeros cambios en el constructor

--- Video.h
+++ Video.h
@@ -1,42 +1,34 @@
-#ifndef Videos
-#define videos
+#ifndef Vide
+#define vide
 #include <iostream>
 #include "Pelicula.h"
 #include "Serie.h"
-using namespace std;
 
-class Video{
-    protected:
-        string genero;
-        string titulo;
-        string ID;
-        float duracion_minutos;
-        float rating;
-    public:
-        Video(string,string,float,float,string,int,int);
-        Video(string, string,float, float, string);
-        string BuscarContenido(string ID);
-        float ObtenerRating(){return rating;};
-        virtual void MostrarVideos()=0;
-        void AñadirRating(float estrellas){};
-        string getGenero(){return genero;};
-        string getTitulo(){return titulo;};
-        void setTitulo(string tit){titulo=tit;};
-        void setGenero(string gnro){genero=gnro;};
+class Video {
+protected:
+    string genero;
+    string titulo;
+    string ID;
+    float duracion_minutos;
+    float rating;
+
+public:
+    Video(string, string, float, float, string);
+    string BuscarContenido(string);
+    float ObtenerRating() { return rating; }
+    virtual void MostrarVideos() = 0;
+    void setRating(float estrellas) { rating = estrellas; }
+    string getGenero() { return genero; }
+    string getTitulo() { return titulo; }
+    void setTitulo(string tit) { titulo = tit; }
+    void setGenero(string gnro) { genero = gnro; }
 };
-Video::Video(string Tit, string gnro,float drcnmin, float estrellas, string identi,int temp,int capi){
-    titulo =Tit;
-    genero=gnro;
-    ID=identi;
-    rating=estrellas;
-    duracion_minutos=drcnmin;
-}
-Video::Video(string Tit, string gnro,float drcnmin, float estrellas, string identi){
-    titulo =Tit;
-    genero=gnro;
-    ID=identi;
-    rating=estrellas;
-    duracion_minutos=drcnmin;
+Video::Video(string Tit, string gnro, float drcnmin, float estrellas, string identi) {
+    titulo = Tit;
+    genero = gnro;
+    ID = identi;
+    rating = estrellas;
+    duracion_minutos = drcnmin;
 }
 
 #endif
