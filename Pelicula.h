4fda1e26eee0f999047ac25afa05cca11d97c868
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-12 08:51:47 -0600

    Update Pelicula.h
    Cambios en el constructor y un método, no debería presentar errores

--- Pelicula.h
+++ Pelicula.h
@@ -2,18 +2,18 @@
 #define Pelicula_
 #include "Video.h"
 
-
-#include <iostream>
-using namespace std;
-class Pelicula : public Video{
-    public:
-    Pelicula(string tit,string gen,float drcn,float rating, string id):Video(tit,gen,drcn,rating,id){};
-    void MostrarVideos();
+class Pelicula : public Video {
+public:
+    Pelicula(string, string, float, float, string);
+    void MostrarVideos() override;
 };
-void Pelicula::MostrarVideos(){
-    cout<<"Titulo: "<<titulo<<"\n";
-    cout<<"Genero: "<<genero<<"\n";
-    cout<<"Rating: "<<rating<<"\n";
-    cout<<"Duración en minutos: "<<duracion_minutos<<"\n";
+Pelicula::Pelicula(string tit, string gen, float drcn, float estrellas, string id)
+    : Video(tit, gen, drcn, estrellas, id) {}
+
+void Pelicula::MostrarVideos() {
+    cout << "Título: " << titulo << "\n";
+    cout << "Género: " << genero << "\n";
+    cout << "Rating: " << rating << "\n";
+    cout << "Duración en minutos: " << duracion_minutos << "\n";
 }
 #endif
