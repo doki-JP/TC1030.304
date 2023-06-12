74a3545e592417521cea92c912e43cdb819ef396
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-12 08:50:21 -0600

    Update Serie.h
    Implementación correcta de Serie y su constructor, no debe presentar errores

--- Serie.h
+++ Serie.h
@@ -2,23 +2,34 @@
 #define Serie_
 #include "Video.h"
 #include "Episodio.h"
-#include <iostream>
 
-using namespace std;
+class Serie : public Video {
+protected:
+    Episodio cap[100];
+    int numC;
 
-class Serie: public Video{
-    protected:
-    Episodio cap;
-    public:
-    Serie(string Tit, string gnro,float drcnmin, float estrellas, string identi):Video(Tit, gnro,0,0,identi){}
-    Serie(string Tit, string gnro,float drcnmin, float estrellas, string identi,int temp,int capi): Video(Tit, gnro,drcnmin,estrellas,identi){cap.AñadirTemp(temp);cap.AñadirCap(capi);};
-    void MostrarVideos();
+public:
+    Serie(string, string, float, float, string, int, int);
+    void MostrarVideos() override;
+    void MostrarCapSerie(string);
 };
-void Serie::MostrarVideos(){
-    cout<<"Titulo: "<<titulo<<"\n";
-    cout<<"Genero: "<<genero<<"\n";
-    cout<<"Rating: "<<rating<<"\n";
-    cout<<"Duración en minutos: "<<duracion_minutos<<"\n";
+Serie::Serie(string Tit, string gnro, float drcnmin, float estrellas, string identi, int temp, int capi)
+    : Video(Tit, gnro, drcnmin, estrellas, identi) {
+    numC = capi;
 }
 
+void Serie::MostrarVideos() {
+    cout << "Título: " << titulo << "\n";
+    cout << "Género: " << genero << "\n";
+    cout << "Rating: " << rating << "\n";
+    cout << "Duración en minutos: " << duracion_minutos << "\n";
+}
+
+void Serie::MostrarCapSerie(string nom) {
+    cout << "Los capítulos de la serie son:\n";
+    for (int i = 0; i < numC; i++) {
+        cap[i].MostrarDatosCap();
+        cout << "\n";
+    }
+}
 #endif
