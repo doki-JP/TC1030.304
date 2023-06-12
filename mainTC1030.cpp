425e08e31fb457e2b7dde93f5aa27b8256485706
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-12 08:54:44 -0600

    Update mainTC1030.cpp
    Este es un main de prueba, para verificar la funcionalidad

--- mainTC1030.cpp
+++ mainTC1030.cpp
@@ -1,9 +1,9 @@
 #include "Video.h"
-#include <iostream>
-int main(){
-    Video *prtS =new Serie("Neon Genesis Evangelion","Accion y Mecha",169,4.25,"S1669",1,26);
-    Video *ptrP = new Pelicula();
+int main() {
+    Serie *prtS = new Serie("Neon Genesis Evangelion", "Accion y Mecha", 169, 4.25, "S01", 1, 26);
+    Pelicula *ptrP = new Pelicula("End of Evangelion", "Accion", 90, 4, "P01");
     ptrP->MostrarVideos();
-    delete(prtS);
-    delete(ptrP);
+    prtS->MostrarCapSerie("Neon Genesis Evangelion");
+    delete prtS;
+    delete ptrP;
 }
