603853dc9e7e1cb9298bd37028933e0cd883050e
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-12 16:48:55 -0600

    Update mainTC1030.cpp
    segundo main de prueba, da mayor claridad al programa y se puede iniciar el proceso de búsqueda de errores

--- mainTC1030.cpp
+++ mainTC1030.cpp
@@ -1,9 +1,21 @@
 #include "Video.h"
-int main() {
-    Serie *prtS = new Serie("Neon Genesis Evangelion", "Accion y Mecha", 169, 4.25, "S01", 1, 26);
-    Pelicula *ptrP = new Pelicula("End of Evangelion", "Accion", 90, 4, "P01");
-    ptrP->MostrarVideos();
+int main(){
+    string nom;
+    float rati;
+    Serie *prtS =new Serie("Neon Genesis Evangelion","Accion y Mecha",169,4.25,"S01",1,26);
+    //Añadir capitulos, puedo hacerlo funcion
+    for (int i=1;i<=prtS->getNumCap();i++){
+        
+        cout<<"Aniade el titulo del capitulo "<<i<<": ";
+        getline(cin,nom);
+        cout<<"Aniade el rating del capitulo: ";
+        cin>>rati;
+        prtS->setCap(i,nom,1,rati);
+    }
     prtS->MostrarCapSerie("Neon Genesis Evangelion");
-    delete prtS;
-    delete ptrP;
+    Pelicula *ptrP = new Pelicula("End of Evangelion","Accion",90,4,"P01");
+    ptrP->MostrarVideos();
+    prtS->MostrarVideos();
+    delete(prtS);
+    delete(ptrP);
 }
