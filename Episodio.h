438416199ae713a6a9de6efc59ba0ecd52514424
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-14 11:46:03 -0600

    Update Episodio.h

--- Episodio.h
+++ Episodio.h
@@ -1,33 +1,34 @@
-#ifndef Episodio_
-#define Episodio_
+#ifndef EPISODIO_H
+#define EPISODIO_H
 #include <iostream>
 using namespace std;
 
 class Episodio {
 private:
-    string NumTemporada;
+    int NumTemporada;
+    int numCapi;
     string Titulo;
     int rating;
 
 public:
     Episodio();
-    Episodio(string, string, int);
+    Episodio(int, string, int, float);
     void MostrarDatosCap();
 };
 Episodio::Episodio() {
     Titulo = "NA";
-    NumTemporada = "-1";
+    NumTemporada = 1;
     rating = 0;
 }
 
-Episodio::Episodio(string tit, string temp, int rat) {
+Episodio::Episodio(int numcap,string tit, int temp, float rat) {
+    numCapi=numcap;
     Titulo = tit;
     NumTemporada = temp;
     rating = rat;
 }
-
 void Episodio::MostrarDatosCap() {
-    cout << "El episodio titulado: " << Titulo << "\n";
+    cout << "El episodio "<<numCapi<<" titulado: " << Titulo << "\n";
     cout << "Tiene un rating de: " << rating << " estrellas\n";
     cout << "Y pertenece a la temporada: " << NumTemporada << "\n";
 }
