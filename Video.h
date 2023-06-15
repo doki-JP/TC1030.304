5110e987d6f97d0f0ec4a544d3d0894dae42ccf5
doki-JP <111583401+doki-JP@users.noreply.github.com>
2023-06-14 21:00:11 -0600

    alpha Video.h
    Se añadieron algunos comentarios, así proporcionando un poco de claridad en el código

--- Video.h
+++ Video.h
@@ -1,3 +1,14 @@
+/* Proyecto Situación problema
+    Juan Pablo Chávez Leal
+    A01705408
+    14/06/23
+    Esta clase es la conglomeración de otras 2, es Video - abstracta y Padre, Serie - Hija, Pelicula - Hija.
+    esta clase es, además de muy sensible a lo que introduzca el usuario, permite que los métodos se usen de la mejor manera posible, en especial con
+    el método abstracto y el uso de polimrfismo entre métodos. 
+
+
+*/
+
 #ifndef VIDEO_H
 #define VIDEO_H
 #include "Episodio.h"
@@ -36,6 +47,8 @@ string Video::BuscarContenido(string name){
 class Pelicula : public Video {
 public:
     Pelicula(string, string, float, float, string);
+    //override() se asegura de que la función que declaramos aquí sea la que se vaya usar en los casos pertinentes, 
+    //así no nos tenemos que preocupar de que no funcione este mmétodo.
     void MostrarVideos() override;
 };
 Pelicula::Pelicula(string tit, string gen, float drcn, float estrellas, string id)
